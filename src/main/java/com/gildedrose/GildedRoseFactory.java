c34bf11aba63217c4469d8290be4ee068bf3f4bb
hussen <hussenadel2000@gmail.com>
2024-09-29 14:58:55 +0300

    feat: organize code adding enum

--- src/main/java/com/gildedrose/GildedRoseFactory.java
+++ src/main/java/com/gildedrose/GildedRoseFactory.java
@@ -1,16 +1,17 @@
 package com.gildedrose;
 
+
 public class GildedRoseFactory {
-        public static MainGildedRose createGildedRose(Item item) {
-            switch (item.name) {
-                case "Aged Brie":
-                    return new AgedGildedRose(item);
-                case "Sulfuras, Hand of Ragnaros":
-                    return new SulfurasGildedRose(item);
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    return new BackStageGildedRose(item);
-                default:
-                    return new DefaultGildedRose(item);
-            }
+
+    public static MainGildedRose createGildedRose(Item item) {
+        if (item.name.equals(ItemName.AGED_BRIE.name)) {
+            return new AgedGildedRose(item);
+        } else if (item.name.equals(ItemName.SULFURAS.name)) {
+            return new SulfurasGildedRose(item);
+        } else if (item.name.equals(ItemName.BACKSTAGE.name)) {
+            return new BackStageGildedRose(item);
+        } else {
+            return new DefaultGildedRose(item);
         }
+    }
 }
---
+++ src/main/java/com/gildedrose/ItemName.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+public enum ItemName {
+
+    AGED_BRIE("Aged Brie"),
+    SULFURAS("Sulfuras, Hand of Ragnaros"),
+    BACKSTAGE("Backstage passes to a TAFKAL80ETC concert");
+
+
+
+
+    final String name;
+
+    ItemName(String name) {
+        this.name = name;
+    }
+
+
+
+}
